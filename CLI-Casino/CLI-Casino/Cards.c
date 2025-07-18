f7a39319508394a25967de5e91f2745c1e6fe659
sebastian <sebastianhsolorzano@gmail.com>
2025-07-18 08:44:31 -0400

    added card struct and definitions for basic linked list functions

---
+++ CLI-Casino/CLI-Casino/Cards.c
@@ -0,0 +1,13 @@
+
+
+
+#include "Cards.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//PDECKNODE initDeck(){}
+
+//void addto(PDECKNODE* existing, CARD c){}
+
+//void removefrom(PDECKNODE* existing, CARD c){}
