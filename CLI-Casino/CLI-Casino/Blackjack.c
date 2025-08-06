8b43571b074c9e66a93acc88111a75b26fa09860
sebastian <sebastianhsolorzano@gmail.com>
2025-08-05 23:44:34 -0400

    Finished!!!!! looks a bit ugly, functions are all over the place, and I haven't commented or removed magic numbers, but blackjack is finished

--- CLI-Casino/CLI-Casino/Blackjack.c
+++ CLI-Casino/CLI-Casino/Blackjack.c
@@ -5,48 +5,51 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <Windows.h>
-#define HAND_MAX 11 // 1+1+1+1+2+2+2+2+3+3+3 = 21, therfore handsize cannot exceed 11
-#define BLACKJACK 21
-#define STARTING_HSIZE 2
+
 #define ROYAL_VAL 10//for tens and aces too
 
+//moves cursor to desired position
 void moveCursor(CURLOC loc) {
 	printf("\033[%d;0H",loc);
-	if (loc == End)
-		printf("\033[J");
+	if (loc == End || loc == Player)
+		printf("\033[J");//clears everything below 
 	if (loc == Status)
-		printf("\033[K");
+		printf("\033[K"); //clears line
 
 }
 
+//prints game menu
 void printBJmenu() {
 	WipeScreen();
 	printf("welcome to blackjack\na. play game\nq. quit to menu\n");
 }
 
 
-
+//runs the game's main loop
 void runBJ(USER u) {
-
 	int bet;
 	FULLDECK fd = initDeck();
-	CARD phand[HAND_MAX];
-	CARD dhand[HAND_MAX];
-
 
 	bool running=true;
 	while (running) {
+
 		printBJmenu();
+
+		//buy-in is done within the game, so the only options are play and quit
 		char input = GetUserInput("aq");
 		switch (input) {
 		case 'a':
+			if (u.balance < 2) {
+				printf("insufficient balance\n");
+				break;
+			}
+
 			bet = getBet(&u);
-			deal(&fd, phand, dhand);
-			int finalphand=playerTurn(&fd,phand,STARTING_HSIZE);
-			int finaldhand=dealerTurn(&fd, dhand, STARTING_HSIZE);
-			payout(&u, bet, phand, finalphand, dhand, finaldhand);
+			playRound(u, &fd, bet);
+			shuffle(&fd);
 			break;
 		case 'q':
+
 			running = false;
 			break;
 		}
@@ -54,35 +57,97 @@ void runBJ(USER u) {
 	return;
 }
 
-void payout(USER* u, int bet, CARD phand[],int phandlen, CARD dhand[], int dhandlen) {
-	int playerScore = scoreHand(phand, phandlen);
-	int dealerScore = scoreHand(dhand, dhandlen);
+
+BJPLAYER initplayer() {
+	BJPLAYER p = { 0 };
+	//array is already initialized? doesn't let me touch it
+	p.handsize = STARTING_HSIZE;
+	p.handsize = STARTING_HSIZE;
+	p.state = Firsthand;
+	p.hasSplit = false;
+
+	return p;
+}
+
+
+void playRound(USER u, FULLDECK* fd, int bet) {
+	BJPLAYER player = initplayer();
+	player.bet = bet; //cant be done in init because of dealer
+	BJPLAYER dealer = initplayer();
+
+	//easiest way I can think of to deal with splits, kinda gross tho
+	BJPLAYER splitp = initplayer();
+	splitp.state = Normal;//splits cant split, bj or dd
+	 
+
+
+
+
+	deal(fd,&player,&dealer);
+	printf("\033[0;0H playerscore: %d, dealerscore: %d   PB: %s  BPJ: %s  DB: %s  DBJ: %s", player.score, dealer.score, (player.state==Busted)?"yes":"no",(player.state==BJed)?"yes":"no", (dealer.state == Busted) ? "yes" : "no", (dealer.state == BJed)?"yes" : "no");
+	moveCursor(End);
+	
+	if (player.state != BJed && dealer.state != BJed) { //avoids continuing if anyone has a natty
+		playerTurn(&u, fd, &player, &splitp);
+		if (player.hasSplit) {
+			moveCursor(Status);
+			printf("playing split 2");
+			playerTurn(&u, fd, &splitp, &splitp);//no default values; so sad :(...
+		}
+		dealerTurn(fd, &dealer);
+	}
+	else
+		Sleep(800);//tension
+	
+	if (player.hasSplit)
+		payout(&u, &splitp, &dealer); //runs split hand first
+	payout(&u, &player,&dealer);
+
+	moveCursor(End);
+}
+
+
+void payout(USER* u,BJPLAYER* p, BJPLAYER* d) {
+	//just in case
+	p->score = scoreHand(p->hand, p->handsize, &p->state);
+	d->score = scoreHand(d->hand, d->handsize, &d->state);
+	moveCursor(Dealer);
+	displayHand(d->hand, d->handsize);
+	moveCursor(Player);
+	displayHand(p->hand, p->handsize);
+
+	if (p->state == Ddowned) {
+		moveCursor(Player);
+		displayHand(p->hand, p->handsize);
+		Sleep(500);//tension
+	}
+
+
+
 	int winnings;
 
-	if (playerScore > BLACKJACK) {
+	if (p->state == Busted) {
 		winnings = 0;
 	}
-	else if (playerScore == dealerScore) {
-
-		winnings = bet;
+	else if (p->score == d->score) {
+		winnings = p->bet;
 	}
-	else if (playerScore == BLACKJACK && phandlen == STARTING_HSIZE) {
-
-		winnings = bet * 2.5;
+	else if (p->state==BJed) {
+		winnings = p->bet * 2.5;
 	}
-	else if (BLACKJACK - playerScore < BLACKJACK - dealerScore || dealerScore>BLACKJACK) {
-
-		winnings = bet * 2;
+	else if (BLACKJACK - p->score < BLACKJACK - d->score || d->state==Busted) {
+		winnings = p->bet * 2;
 	}
 	else {
-
 		winnings = 0;
 	}
 
-
+	printf("\033[1;1H bet %d", p->bet);
 	moveCursor(Status);
-	printf("Won %d \na. payout\n", winnings);
-	u += winnings;
+	printf("Won % d",winnings);
+	u->balance += winnings;
+	moveCursor(End); 
+	printf("a.payout\n");
 	int in = GetUserInput("a");
 }
 
@@ -110,57 +175,69 @@ int getBet(USER* u) {
 }
 
 
-void deal(FULLDECK* fd, CARD phand[], CARD dhand[]) {
-	phand[0] = drawCard(fd);
-	dhand[0] = drawCard(fd);
-	phand[1] = drawCard(fd);
-	dhand[1] = fd->facedown;
-	CARD dealersecret = drawCard(fd);
+void deal(FULLDECK* fd, BJPLAYER* p, BJPLAYER* d) {
+	p->hand[0] = drawCard(fd);
+	d->hand[0] = drawCard(fd);
+	p->hand[1] = drawCard(fd);
+	d->hand[1] = fd->facedown;//dealer card stays hidden
 
 	WipeScreen();
 	Sleep(100);
-	displayHand(dhand, STARTING_HSIZE);
+	moveCursor(Dealer);
+	displayHand(d->hand, d->handsize);
 	Sleep(400);
-	displayHand(phand, STARTING_HSIZE);
-	if(getValue(dhand[0])>=ROYAL_VAL)
-		Sleep(700);
+	moveCursor(Player);
+	displayHand(p->hand, p->handsize);
 
-	CARD temp[] = { dhand[0],dealersecret };
-	if (scoreHand(temp, STARTING_HSIZE) == BLACKJACK) {//only possible with nattys
-		dhand[1] = dealersecret;
-		moveCursor(Dealer);
-		displayHand(dhand, STARTING_HSIZE);
-		moveCursor(End);
+	//now that we're done printing we can just add dealer's second card to hand, screen won't be updated until dealerturn anyway
+	d->hand[1] = drawCard(fd);
+
+
+	//we don't actually really care about score at this point, but taking it just makes things simpler
+	//what we really want is status, since game ends early if anyone has bj
+	d->score=scoreHand(d->hand, d->handsize, &d->state);
+	p->score=scoreHand(p->hand, p->handsize, &p->state);
+
+
+
+	//if dealer has a possibility of blackjack
+	if (getValue(d->hand[0]) >= ROYAL_VAL) {//cant use score here because its specifically about the first card
+		Sleep(700);//tension
+
+		if (d->state == BJed) {
+			moveCursor(Dealer);
+			displayHand(d->hand, STARTING_HSIZE);
+			moveCursor(End);
+		}
+		else {
+			moveCursor(Status);
+			printf("dealer does not have blackjack\n");
+			moveCursor(End);
+		}
 	}
-	else if (getValue(dhand[0]) >= ROYAL_VAL) {
-		moveCursor(Status);
-		printf("dealer does not have blackjack\n");
-		moveCursor(End);
-	}
-	dhand[1] = dealersecret;
 	return;
 }
 
-void availableMoves(CARD hand[], int len, char options[]) {
+void availableMoves(BJPLAYER* p, char options[]) {
 	bool canhit = false;
 	bool canstand = true;//always true
 	bool cansplit = false;
 	bool canddown = false;
 	int n = 0;
 
-	if (scoreHand(hand, len) < BLACKJACK) {
+	if (p->state!=Busted) {
 		canhit = true;
 		options[n] = 'a';
 		n++;
 	}
 	options[n] = 'b';
 	n++;
-	if (hand[0].ran == hand[1].ran) {
+	if (p->hand[0].ran == p->hand[1].ran && p->state==Firsthand) {
 		cansplit = true;
 		options[n] = 'c';
 		n++;
 	}
-	if (scoreHand(hand, len) > 8 && scoreHand(hand, len) < 12) {
+	if (p->score > 8 && p->score < 12 && p->state==Firsthand) {
 		canddown = true;
 		options[n] = 'd';
 		n++;
@@ -180,74 +257,112 @@ void availableMoves(CARD hand[], int len, char options[]) {
 	return options;
 }
 
-int playerTurn(FULLDECK* fd,CARD hand[], int len) {
+void playerTurn(USER* u, FULLDECK* fd,BJPLAYER* p,BJPLAYER* splip) {
 	moveCursor(Player);
-	displayHand(hand, len);
+	displayHand(p->hand, p->handsize);
 	moveCursor(End);
 
-	if (scoreHand(hand, len) == BLACKJACK && len==STARTING_HSIZE) {
-		moveCursor(Status);
-		printf("BlackJack!\n");
-		return len;
-	}
-	else if (scoreHand(hand, len) > BLACKJACK) {
+	//since this can loop multiple times we should take it at the start
+	p->score = scoreHand(p->hand, p->handsize, &p->state);
+
+	if (p->state==Busted) {
 		moveCursor(Status);
 		printf("Busted...\n");
-		return len;
+		return;
 	}
 
 
 	char options[5];
-	availableMoves(hand, len, options);
+	availableMoves(p, options);
 	int input = GetUserInput(options);
 	switch (input) {
 	case 'a':
-		hand[len] = drawCard(fd);
-		len=playerTurn(fd, hand, len + 1);
+		p->hand[p->handsize] = drawCard(fd);
+		p->handsize++;
+		p->state = Normal;
+		playerTurn(u,fd, p,splip); //eww recursion, I know, but this seemed like a easier way of doing it
+			//spli wont get used at all but needed because no default values in c
 		break;
 	case 'b':
 		break;
 	case 'c':
-		printf("subtracting equal bet\n");
+		moveCursor(Status);
+		printf("playing split 1");
+
+		//for split hand
+		splip->hand[0] = p->hand[1];
+		splip->hand[1] = drawCard(fd);
+		splip->bet = p->bet;
+		u->balance -= p->bet;
+
+		//continuing to play first hand
+		p->hand[1] = drawCard(fd); //not incrementing because we're replacing the card we had
+		p->state = Normal;
+		p->hasSplit = true;
+		playerTurn(u, fd, p, splip); //eww recursion, I know, but this seemed like a easier way of doing it
+			//spli wont get used at all but needed because no default values in c
 
 		break;
 	case 'd':
-		printf("doubling");
+		moveCursor(Status);
+		printf("doubled down");
+		u->balance -= p->bet;
+		p->bet += p->bet;
+		
+		//visuals for facedown card
+		p->hand[p->handsize] = fd->facedown;
+		moveCursor(Player);
+		displayHand(p->hand, p->handsize+1);
+		moveCursor(End);
+		//actual card and status
+		p->hand[p->handsize] = drawCard(fd);
+		p->handsize++;//we didn't raise it earlier because we want to replace the facedown card
+		p->state = Ddowned;
+
 		break;
 	}
-	return len;
 }
 
-int dealerTurn(FULLDECK* fd,CARD hand[], int len) {
-	Sleep(500);
+void dealerTurn(FULLDECK* fd,BJPLAYER* d) {
+	Sleep(500);//tension
+
+	//reveals their hidden card
 	moveCursor(Dealer);
-	displayHand(hand, len);
-	moveCursor(End);
+	displayHand(d->hand, d->handsize);
 
-	while (scoreHand(hand, len) < 17) {
-		hand[len] = drawCard(fd);
-		len++;
-		moveCursor(Dealer);
-		Sleep(500);
-		displayHand(hand, len);
-		moveCursor(End);
+	while (d->score < 17) {
+		Sleep(500);//more tension
+		moveCursor(Dealer);//to overwrite previously drawn
+		d->hand[d->handsize] = drawCard(fd);
+		d->handsize++;
+		displayHand(d->hand, d->handsize);
+		d->score = scoreHand(d->hand, d->handsize, &d->state);
 	}
-	return len;
+	moveCursor(End);
 }
 
-int scoreHand(CARD hand[], int len) {
+//not accepting a player struct directly because then it gets annoying with hidden cards and the like
+int scoreHand(CARD hand[], int len, STAT* s) {
 	int score=0;
 	bool hasAce = false;
 
 	for (int i = 0; i < len;i++) {
 		if (hand[i].ran == Ace) 
 			hasAce = true;
+
 		score += getValue(hand[i]);
+
 		if (score > BLACKJACK && hasAce == true) {
 			score -= ROYAL_VAL;//stripping it of its royalty
 			hasAce = false; //in case of multiple aces
 		}
 	}
+
+	if (*s == Firsthand && score == BLACKJACK) //notably, blackjackwont be counted on splits
+		*s = BJed;
+	else if (score > BLACKJACK)
+		*s = Busted;
+
 	return score;
 }
 
--- CLI-Casino/CLI-Casino/Blackjack.h
+++ CLI-Casino/CLI-Casino/Blackjack.h
@@ -4,25 +4,57 @@
 #include "IOController.h"
 #include "Cards.h"
 #include "User.h"
+#include <stdbool.h>
+#define HAND_MAX 11 // 1+1+1+1+2+2+2+2+3+3+3 = 21, therfore handsize cannot exceed 11
+#define BLACKJACK 21
+#define STARTING_HSIZE 2
 
+
+typedef enum playerStatus {
+	Firsthand,
+	BJed,
+	Normal,
+	Ddowned,
+	Busted
+}STAT;
+
+//for both players and dealer, though dealer doesn't use second hand
+typedef struct bjplayer {
+	CARD hand[HAND_MAX];
+	int handsize;
+	int score;
+	int bet; //dealer has this parameter but doesn't use it
+	STAT state;
+	bool hasSplit; //cant be a state because it can get overriden by busting
+
+
+}BJPLAYER;
 
 typedef enum cursorLocation {
 	Dealer = 3,
-	Player = 20,
-	Status =37,
+	Player = 21,
+	Status =20,
 	End=38
 }CURLOC;
+
+
+BJPLAYER initplayer();
 
 void runBJ(USER u);
 
 int getBet(USER* u);
 
-void deal(FULLDECK* fd, CARD phand[], CARD dhand[]);
+void deal(FULLDECK* fd, BJPLAYER* p, BJPLAYER* d);
 
-int playerTurn(FULLDECK* fd, CARD hand[], int len);
+void playerTurn(USER* u, FULLDECK* fd, BJPLAYER* p, BJPLAYER* splip);
 
-int scoreHand(CARD hand[], int len);
+int scoreHand(CARD hand[], int len, STAT* s);
 
-int dealerTurn(FULLDECK* fd, CARD hand[], int len);
+void availableMoves(BJPLAYER* p, char options[]);
 
-void payout(USER* u, int bet, CARD phand[], int phandlen, CARD dhand[], int dhandlen);
+
+void dealerTurn(FULLDECK* fd, BJPLAYER* d);
+
+void payout(USER* u, BJPLAYER* p, BJPLAYER* d);
+
+void playRound(USER u, FULLDECK* fd, int bet);
